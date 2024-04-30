1ef0ea19e6256a780b1770cb2e766bc61d500e74
0xtyc <tyc.tingyuchen@gmail.com>
2024-05-01 00:16:40 +0200

    Rust basics exercises: if & fn

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -3,13 +3,13 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num // or return num * num; 
+    // the last expression is automatically returned in rust 
+    // but after adding a semicolon, it becomes a statement and does not return a value
 }
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,10 +3,9 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 3;
+    // variable bindings are immutable by default in Rust
+    let mut x = 3; // set mutable to mutate it later
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
